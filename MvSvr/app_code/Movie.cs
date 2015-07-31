c48cf6a0874d52ed05b136deca66f56442252532
handshou <heyhansel@outlook.com>
2015-08-01 01:11:26 +0800

    Added 4 new methods to Movie.cs

--- MvSvr/app_code/Movie.cs
+++ MvSvr/app_code/Movie.cs
@@ -7,12 +7,13 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 
 namespace MvSvr {
 
     [Serializable()]
 
-    public class Movie {
+    public class Movie : IComparer<Show> {
 
         // Attributes + Get Set
         public String Title { get; set; }
@@ -49,14 +50,51 @@ namespace MvSvr {
         }
 
         // Methods
-        //public List<Show> FindMovies(String date) {
-        //// returns all shows that fall on given date
+        public List<Show> FindShows(int index) {
+            // returns all shows that fall on given date by index
+            List<Show> shows = new List<Show>();
+            for (int i = 0; i < Shows.Count; i++) {
+                String sdate = Shows[i].Date;
+                if (sdate.Equals(GetDates()[index])) {
+                    shows.Add(Shows[i]);
+                }
+            }
+            shows.OrderBy(x => x.TimeStart);
 
-        //}
+            return shows;
+        }
+        public List<Show> FindShows(String date) {
+            // returns all shows that fall on given date by date
+            List<Show> shows = new List<Show>();
+            for (int i = 0; i < Shows.Count; i++) {
+                if (Shows[i].Date.ToLower().Equals(date)) {
+                    shows.Add(Shows[i]);
+                }
+            }
+            shows.OrderBy(x => x.TimeStart);
 
-        //public List<Show> FindMovies(String date, String timeStart) {
-        //// returns all shows that fall on given date and time
+            return shows;
+        }
+        public List<String> GetDates() {
+            List<String> showdates = new List<String>();
+            foreach (Show s in Shows) {
+                if (!showdates.Contains(s.Date))
+                    showdates.Add(s.Date);
+            }
+            showdates.Sort();
 
-        //}
+            return showdates;
+        }
+        public List<String> FindShowTimes(String date) {
+            List<String> showtimes = new List<String>();
+            showtimes = new List<String>();
+            for (int i = 0; i < Shows.Count; i++) {
+                if (Shows[i].Date.Equals(date))
+                    showtimes.Add(Shows[i].TimeStart);
+            }
+            showtimes.Sort();
+
+            return showtimes;
+        }
     }
 }
